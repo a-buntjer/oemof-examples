279f9cc23798ad3cd735931dba367bfc15c74509
Arne Buntjer <arne.buntjer@tuhh.de>
2019-10-16 16:08:48 +0200

    Added for loop over time intervals for rolling_horizon.

--- oemof_examples/oemof.solph/v0.3.x/rolling_horizon/rolling_horizon_hot_warm_cold_start.py
+++ oemof_examples/oemof.solph/v0.3.x/rolling_horizon/rolling_horizon_hot_warm_cold_start.py
@@ -13,7 +13,7 @@ from oemof.network import Node
 from oemof.solph import (EnergySystem, Model, Flow, Source, Sink, Bus,
                          RollingHorizon)
 from oemof.outputlib import processing, views
-
+from time import time
 # read sequence data
 full_filename = os.path.join(os.path.dirname(__file__),
                              'data.csv')
@@ -21,10 +21,12 @@ data = pd.read_csv(full_filename, sep=";")
 
 # select periods
 periods = 24
+total_time_steps = 8760
 
 # create an energy system
-idx = pd.date_range('1/1/2017', periods=periods, freq='H')
-es = EnergySystem(timeindex=idx)
+idx = pd.date_range('1/1/2017', periods=total_time_steps, freq='H')
+start = time()
+es = EnergySystem(timeindex=idx[:periods])
 Node.registry = es
 
 # power bus and components
@@ -43,41 +45,60 @@ pp2 = Source(
     label='power_plant2',
     outputs={
         bel: Flow(
-            nominal_value=10, min=0.3, max=1.0, variable_costs=5,
+            nominal_value=10, min=0.2, max=1.0, variable_costs=5,
             rollinghorizon=RollingHorizon(t_start_cold=5,
                                           t_start_warm=3,
                                           t_start_hot=1,
                                           cold_start_costs=5,
                                           warm_start_costs=4,
                                           hot_start_costs=1,
-                                          T_int=24,
-                                          T=0,
                                           flow_min_last=0.3,
                                           minimum_downtime=1,
-                                          optimized_status=data.loc[:periods-1, 'optimized_status'].tolist(),
-                                          optimized_flow=data.loc[:periods-1, 'optimized_flow'].tolist()))})
+                                          T_int=periods-1))})
 
+print(f'Time for es creation: {time()-start}')
 # create an optimization problem and solve it
-om = Model(es)
+start = time()
+total_optimization_period = [x for x in range(24, 8760, periods)]
+for T in total_optimization_period:
+    om = Model(es)
+    om.solve(solver='cplex', solve_kwargs={'tee': True})
+    for (i, o) in om.FLOWS:
+        for t in om.TIMESTEPS:
+            if om.flows[i, o].actual_value[t] is not None:
+                # pre- optimized value of flow variable
+                om.flows[i, o].actual_value[t] = data.loc[T+t, 'demand_el']
+                om.flow[i, o, t].value = (
+                    om.flows[i, o].actual_value[t] *
+                    om.flows[i, o].nominal_value)
+                # fix variable if flow is fixed
+                if om.flows[i, o].fixed:
+                    om.flow[i, o, t].fix()
+        if om.flows[i, o].rollinghorizon:
+            om.flows[i, o].rollinghorizon.T = T
+            om.flows[i, o].rollinghorizon.optimized_status[T-periods:T] = list(om.RollingHorizonFlow.status[i, o, :]())
+    es.timeindex = idx[T-periods:T]
+print(f'Time for om creation: {time()-start:.6f}')
+
 
 # debugging
-om.write('problem.lp', io_options={'symbolic_solver_labels': True})
+# om.write('problem.lp', io_options={'symbolic_solver_labels': True})
 
 # solve model
-om.solve(solver='cplex', solve_kwargs={'tee': True})
+#om.solve(solver='cplex', solve_kwargs={'tee': True})
 
 # create result object
-results = processing.results(om)
-
-# plot result_data
-if plt is not None:
-    # plot electrical bus
-    result_data = views.node(results, 'bel')['sequences']
-    result_data[(('bel', 'demand_el'), 'flow')] *= -1
-    columns = [c for c in result_data.columns
-               if not any(s in c for s in ['status'])]
-    result_data = result_data[columns]
-    ax = result_data.plot(kind='line', drawstyle='steps-post', grid=True, rot=0)
-    ax.set_xlabel('Hour')
-    ax.set_ylabel('P (MW)')
-    plt.show()
+#results = processing.results(om)
+#
+## plot result_data
+#if plt is not None:
+#    # plot electrical bus
+#    result_data = views.node(results, 'bel')['sequences']
+#    result_data[(('bel', 'demand_el'), 'flow')] *= -1
+#    columns = [c for c in result_data.columns
+#               if not any(s in c for s in ['status'])]
+#    result_data = result_data[columns]
+#    ax = result_data.plot(kind='line', drawstyle='steps-post', grid=True, rot=0)
+#    ax.set_xlabel('Hour')
+#    ax.set_ylabel('P (MW)')
+#    plt.show()
